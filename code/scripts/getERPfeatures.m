06ddbaa48797e75c8da2da56175ede26be1b5a19
James Desjardins <james.desjardins@computeontario.ca>
2019-08-07 12:24:19 -0400

    add area measures and time intervals to getERPfeatures.m

--- code/scripts/getERPfeatures.m
+++ code/scripts/getERPfeatures.m
@@ -32,14 +32,31 @@ temporal_labs={'FT8','TP8','FT7','TP7'};
 
 fid=fopen(outfname,'w');
 fprintf(fid,'%s',['fname,',...
-                'frontal_area,central_area,posterior_area,temporal_area,', ...
-                'frontal_peak,central_peak,posterior_peak,temporal_peak,', ...
-                'frontal_peak_lat,central_peak_lat,posterior_peak_lat,temporal_peak_lat']);
+                'frontal_m_area,central_m_area,posterior_m_area,temporal_m_area,', ...
+                'frontal_rms_area,central_rms_area,posterior_rms_area,temporal_rms_area,', ...
+                'frontal_t_area,central_t_area,posterior_t_area,temporal_t_area,', ...
+                'frontal_peak_p,central_peak_p,posterior_peak_p,temporal_peak_p,', ...
+                'frontal_peak_p_lat,central_peak_p_lat,posterior_peak_p_lat,temporal_peak_p_lat,', ...
+                'frontal_peak_n,central_peak_n,posterior_peak_n,temporal_peak_n,', ...
+                'frontal_peak_n_lat,central_peak_n_lat,posterior_peak_n_lat,temporal_peak_n_lat,',...
+                'frontal_t1_peak_p,central_t1_peak_p,posteriort_t1_peak_p,temporal_t1_peak_p,', ...
+                'frontal_t1_peak_p_lat,central_t1_peak_p_lat,posterior_t1_peak_p_lat,temporal_t1_peak_p_lat,', ...
+                'frontal_t2_peak_p,central_t2_peak_p,posteriort_t2_peak_p,temporal_t2_peak_p,', ...
+                'frontal_t2_peak_p_lat,central_t2_peak_p_lat,posterior_t2_peak_p_lat,temporal_t2_peak_p_lat,', ...
+                'frontal_t3_peak_p,central_t3_peak_p,posteriort_t3_peak_p,temporal_t3_peak_p,', ...
+                'frontal_t3_peak_p_lat,central_t3_peak_p_lat,posterior_t3_peak_p_lat,temporal_t3_peak_p_lat,', ...
+                'frontal_t4_peak_p,central_t4_peak_p,posteriort_t4_peak_p,temporal_t4_peak_p,', ...
+                'frontal_t4_peak_p_lat,central_t4_peak_p_lat,posterior_t4_peak_p_lat,temporal_t4_peak_p_lat']);
 
 for fi=1:length(c_fname);
     fprintf(fid,'\n%s,',c_fname{fi});
     EEG=pop_loadset('filename',c_fname{fi});
     EEG=eeg_checkset(EEG);
+    
+    frontal_inds=[];
+    central_inds=[];
+    posterior_inds=[];
+    temporal_inds=[];
     
     for li=1:length(frontal_labs);
         frontal_inds(li)=find(strcmp({EEG.chanlocs.labels},frontal_labs{li}));
@@ -55,28 +72,75 @@ for fi=1:length(c_fname);
     end
     
     ERP=mean(EEG.data,3);
+    ERP_r=[];
     ERP_r(1,:)=mean(ERP(frontal_inds,:),1);
     ERP_r(2,:)=mean(ERP(central_inds,:),1);
     ERP_r(3,:)=mean(ERP(posterior_inds,:),1);
     ERP_r(4,:)=mean(ERP(temporal_inds,:),1);
     
-    %0 to 800
-    t_inds=find(EEG.times>0 & EEG.times<800);
+    %0 to 789
+    t_inds=find(EEG.times>0 & EEG.times<=789);
     
     % measures
     % area [mean amplitude]
-    area=mean(ERP_r(:,t_inds),2);
+    m_area=mean(ERP_r(:,t_inds),2);
+    rms_area=sqrt(mean(ERP_r(:,t_inds).^2,2));
+    t_area=trapz(ERP_r(:,t_inds),2);
     
     % peakes
     %positive
-    [peak_amp,peak_lat_ind]=max(ERP_r(:,t_inds),[],2);
-    
+    [peak_p_amp,peak_p_lat_ind]=max(ERP_r(:,t_inds),[],2);    
+    peak_p_lat=[];
     for ai=1:size(ERP_r,1);
-        peak_lat(ai)=EEG.times(t_inds(peak_lat_ind(ai)));
+        peak_p_lat(ai)=EEG.times(t_inds(peak_p_lat_ind(ai)));
     end
     
+    %negative
+    [peak_n_amp,peak_n_lat_ind]=min(ERP_r(:,t_inds),[],2);    
+    peak_n_lat=[];
+    for ai=1:size(ERP_r,1);
+        peak_n_lat(ai)=EEG.times(t_inds(peak_n_lat_ind(ai)));
+    end
+    
+
+    %100 to 219
+    t1_inds=find(EEG.times>=100 & EEG.times<=219);
+    [t1_peak_p_amp,t1_peak_p_lat_ind]=max(ERP_r(:,t1_inds),[],2);    
+    t1_peak_p_lat=[];
+    for ai=1:size(ERP_r,1);
+        t1_peak_p_lat(ai)=EEG.times(t1_inds(t1_peak_p_lat_ind(ai)));
+    end
+    
+    %220 to 319
+    t2_inds=find(EEG.times>=220 & EEG.times<=319);
+    [t2_peak_n_amp,t2_peak_n_lat_ind]=min(ERP_r(:,t2_inds),[],2);    
+    t2_peak_n_lat=[];
+    for ai=1:size(ERP_r,1);
+        t2_peak_n_lat(ai)=EEG.times(t2_inds(t2_peak_n_lat_ind(ai)));
+    end
+
+    %320 to 540
+    t3_inds=find(EEG.times>=320 & EEG.times<=540);
+    [t3_peak_p_amp,t3_peak_p_lat_ind]=max(ERP_r(:,t3_inds),[],2);    
+    t3_peak_p_lat=[];
+    for ai=1:size(ERP_r,1);
+        t3_peak_p_lat(ai)=EEG.times(t3_inds(t3_peak_p_lat_ind(ai)));
+    end
+    
+    %541 to 789
+    t4_inds=find(EEG.times>=541 & EEG.times<=789);
+    [t4_peak_p_amp,t4_peak_p_lat_ind]=max(ERP_r(:,t4_inds),[],2);    
+    t4_peak_p_lat=[];
+    for ai=1:size(ERP_r,1);
+        t4_peak_p_lat(ai)=EEG.times(t4_inds(t4_peak_p_lat_ind(ai)));
+    end
+
     
     % print current input file measures
-    fprintf(fid,'%6.3f,',[area',peak_amp',peak_lat]);
+    fprintf(fid,'%6.3f,',[m_area',rms_area',t_area',peak_p_amp',peak_p_lat,peak_n_amp',peak_n_lat, ...
+                            t1_peak_p_amp',t1_peak_p_lat, ...
+                            t2_peak_n_amp',t2_peak_n_lat, ...
+                            t3_peak_p_amp',t3_peak_p_lat, ...
+                            t4_peak_p_amp',t4_peak_p_lat,]);
     
 end
